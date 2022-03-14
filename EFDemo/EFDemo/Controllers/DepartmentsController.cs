ee0c4b3a471b88020dddb15924e0491e5da8835c
geethasamynathan <geethasamynathan2011@gmail.com>
2022-03-14 11:30:30 +0530

    EF demo

---
+++ EFDemo/EFDemo/Controllers/DepartmentsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDemo.Models;
+namespace EFDemo.Controllers
+{
+    public class DepartmentsController : Controller
+    {
+        MVC_DBEntities  entities = new MVC_DBEntities();
+        // GET: Departments
+        public ActionResult Index()
+        {
+
+            return View(entities.Departments.ToList());
+        }
+    }
+}
---
+++ EFDemo/EFDemo/Controllers/EmployeesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EFDemo.Models;
+namespace EFDemo.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        MVC_DBEntities entities=new MVC_DBEntities();
+        // GET: Employees
+        //public ActionResult Index()
+        //{
+            
+        //    return View(entities.Employees.ToList());
+        //}
+        public ActionResult Index(int departmentid)
+        {
+            var employees = entities.Employees.Where(emp => emp.departmentid == departmentid).ToList();
+            return View(employees);
+        }
+        public ActionResult EmplList()
+        {
+
+            return View(entities.Employees.ToList());
+        }
+        public ActionResult Details(int id)
+        {
+           
+            Employee employee = entities.Employees.FirstOrDefault(e => e.Id == id);
+            return View(employee);
+        }
+    }
+}
